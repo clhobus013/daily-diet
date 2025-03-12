bf05a493219288521e87857481447f1a14d40097
Claudia Hobus <clauu013@gmail.com>
2025-03-12 17:04:18 -0300

    Configura tema

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    NunitoSans_400Regular, NunitoSans_700Bold
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Text>Teste</Text> :  <ActivityIndicator/>}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
