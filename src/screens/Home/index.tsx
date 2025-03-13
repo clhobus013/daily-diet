e53590393d2e008a26448597b80d9eee8003ad9e
Claudia Hobus <clauu013@gmail.com>
2025-03-13 17:00:20 -0300

    Finaliza tela de home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,43 @@
-import { Container } from "./styles";
+import { SectionList} from "react-native";
+import { Container, MealSectionHeader, Text } from "./styles";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { DietProgress } from "@components/DietProgress";
+import { Button } from "@components/Button";
+import { MealItem } from "@components/MealItem/intex";
 
 export function Home() {
+
+    const sections = [
+        {
+          title: '12.08.22',
+          data: ['X-tudo', 'Whey', 'Salada'],
+        },
+        {
+          title: '11.08.22',
+          data: ['X-tudo', 'Whey', 'Salada'],
+        },
+      ];
+
     return(
         <Container>
             <HomeHeader/>
             <DietProgress/>
+
+            <Text>Refeicões</Text>
+            <Button/>
+
+            <SectionList
+                sections={sections}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => (
+                    <MealItem/>
+                )}
+                renderSectionHeader={({section: {title}}) => (
+                    <MealSectionHeader>{title}</MealSectionHeader>
+                )}
+
+            />
         </Container>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,26 @@
-import styled from "styled-components/native";
+import styled, {css} from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
     padding: 24px;
     background-color: ${({theme}) => theme.COLORS.WHITE};
+`;
+
+export const Text = styled.Text`
+    ${({theme}) => css`
+        color: ${theme.COLORS.GRAY_100};
+        font-family: ${theme.FONT_FAMILY.REGULAR};
+        font-size: ${theme.FONT_SIZE.MD}px;
+    `}
+
+    margin-bottom: 8px;
+`;
+
+export const MealSectionHeader = styled.Text`
+    ${({theme}) => css`
+        color: ${theme.COLORS.GRAY_100};
+        font-family: ${theme.FONT_FAMILY.BOLD};
+        font-size: ${theme.FONT_SIZE.LG}px;
+    `}
+    margin-top: 32px;
 `;
