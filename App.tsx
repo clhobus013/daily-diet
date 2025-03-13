c3ccabcd5063b023db7a327b8cd204f54cb8b954
Claudia Hobus <clauu013@gmail.com>
2025-03-13 11:39:11 -0300

    Configura path mapping

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
-import { ActivityIndicator, Text } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 
-import theme from './src/theme';
+import theme from '@theme/index';
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
+import { Home } from '@screens/Home';
 
 export default function App() {
-  const [fontsLoaded, error] = useFonts({
+  const [fontsLoaded] = useFonts({
     NunitoSans_400Regular, NunitoSans_700Bold
   });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Teste</Text> :  <ActivityIndicator/>}
     </ThemeProvider>
   );
