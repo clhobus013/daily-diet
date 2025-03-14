02743994b17c582945377654a372661db90e3049
Claudia Hobus <clauu013@gmail.com>
2025-03-14 20:16:58 -0300

    Add radioInput component

---
+++ src/components/RadioInput/index.tsx
@@ -0,0 +1,31 @@
+import { useTheme } from "styled-components/native";
+import { Circle } from "phosphor-react-native";
+import { Container, FormRadio, Label } from "./styles";
+
+import { Button } from "@components/Button";
+
+export function RadioInput() {
+
+    const theme = useTheme();
+    return (
+        <Container>
+            <Label>Está dentro da dieta?</Label>
+
+            <FormRadio>
+                <Button variant="success">
+                    <Button.Icon icon={Circle} weight="fill" color={theme.COLORS.GREEN_DARK} size="8px"/>
+                    <Button.Title>
+                        Sim
+                    </Button.Title>
+                </Button>
+
+                <Button variant="success">
+                    <Button.Icon icon={Circle} weight="fill" color={theme.COLORS.RED_DARK} size="8px"/>
+                    <Button.Title>
+                        Não
+                    </Button.Title>
+                </Button>
+            </FormRadio>
+        </Container>
+    )
+}
