1807d2f7336c1ae1df4ab6cfabe0ba0c9785dfdb
Claudia Hobus <clauu013@gmail.com>
2025-03-18 18:11:40 -0300

    Add header na pagina de estatistica

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,21 +1,31 @@
-import { Container, StatRow, Title } from "./styles";
+import { Container, Content, Header, IconButton, ScoreDescription, ScoreValue, StatRow, Title } from "./styles";
 import { StatCard } from "@components/StatCard";
+import { TouchableOpacity } from "react-native";
 
 
 export function Statistics() {
   return (
     <Container>
-      <Title>Estatísticas gerais</Title>
-      <StatRow>
-          <StatCard/>
-      </StatRow>
-      <StatRow>
-          <StatCard/>
-      </StatRow>
-      <StatRow>
-          <StatCard/>
-          <StatCard/>
-      </StatRow>
+      <Header>
+        <TouchableOpacity>
+          <IconButton/>
+        </TouchableOpacity>
+        <ScoreValue>90%</ScoreValue>
+        <ScoreDescription>das refeiçoes dentro da dieta</ScoreDescription>
+      </Header>
+      <Content>
+        <Title>Estatísticas gerais</Title>
+        <StatRow>
+            <StatCard/>
+        </StatRow>
+        <StatRow>
+            <StatCard/>
+        </StatRow>
+        <StatRow>
+            <StatCard/>
+            <StatCard/>
+        </StatRow>
+      </Content>
     </Container>
   );
 }
--- src/screens/Statistics/styles.ts
+++ src/screens/Statistics/styles.ts
@@ -1,8 +1,45 @@
+import { ArrowLeft } from "phosphor-react-native";
 import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
+  padding-top: 20px;
+  background-color: ${({theme}) => theme.COLORS.GREEN_LIGHT};
+`;
+
+export const Header = styled.View`
   padding: 24px;
+`;
+
+export const IconButton = styled(ArrowLeft)`
+  color: ${({theme}) => theme.COLORS.GREEN_DARK};
+`;
+
+export const ScoreValue = styled.Text`
+  ${({theme}) => css`
+    font-size: ${theme.FONT_SIZE.XXL}px;
+    color: ${theme.COLORS.GRAY_100};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+
+  text-align: center;
+`;
+
+export const ScoreDescription = styled.Text`
+  ${({theme}) => css`
+    font-size: ${theme.FONT_SIZE.SM}px;
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
+
+  text-align: center;
+`;
+
+export const Content = styled.View`
+  height: 100%;
   gap: 12px;
+  padding: 24px;
+  border-radius: 20px;
+  background-color: ${({theme}) => theme.COLORS.WHITE};
 `;
   
 export const Title = styled.Text`
