4444b42c11f24804ecb3cf0932242e01ee8fb2b1
Claudia Hobus <clauu013@gmail.com>
2025-03-20 12:04:24 -0300

    Add tipagem navegacao

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            home: undefined;
+            feedback: undefined;
+            mealDetails: undefined;
+            mealForm: undefined;
+            statistics: undefined;
+        }
+    }
+}
