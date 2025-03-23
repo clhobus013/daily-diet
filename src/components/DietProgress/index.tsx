b70735c6c14f027ed3e587f5470be698273b8374
Claudia Hobus <clauu013@gmail.com>
2025-03-23 14:28:54 -0300

    Add params ao dietProgress

--- src/components/DietProgress/index.tsx
+++ src/components/DietProgress/index.tsx
@@ -1,21 +1,20 @@
 import { TouchableOpacityProps } from "react-native";
-import { Container, Icon, IconButton, Progress, Text } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { Container, Icon, IconButton, Progress, Text } from "./styles";
 
 type Props = TouchableOpacityProps & {
-
+    progress: number;
 }
 
-export function DietProgress({...rest}: Props) {
-
+export function DietProgress({progress, ...rest}: Props) {
     const navigation = useNavigation();
 
     return(
-        <Container>
+        <Container progress={progress}>
             <IconButton {...rest} onPress={() => navigation.navigate("statistics")}>
-                <Icon/>
+                <Icon progress={progress}/>
             </IconButton>
-            <Progress>90,86%</Progress>
+            <Progress>{progress.toFixed(2).replace(".", ",")}%</Progress>
             <Text>das refeições dentro da dieta</Text>
         </Container>
     )
