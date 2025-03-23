1f56c2c54e4f21d95790e71d19950ee22d30cc4c
Claudia Hobus <clauu013@gmail.com>
2025-03-23 14:28:10 -0300

    Add AlertModal component

--- src/components/AlertModal/index.tsx
+++ src/components/AlertModal/index.tsx
@@ -10,19 +10,19 @@ type Props = {
 
 export function AlertModal({visible, onSuccess, onCancel}: Props) {
     return (
-        <Modal visible={visible} transparent style={{backgroundColor: 'blue'}}>
+        <Modal visible={visible} transparent>
             <Overlay>
-            <Container>
-                <Title>Deseja realmente excluir o registro da refeição ?</Title>
-                <ButtonsView>
-                    <Button variant="secondary" onPress={onCancel}>
-                        <Button.Title>Cancelar</Button.Title>
-                    </Button>
-                    <Button onPress={onSuccess}>
-                        <Button.Title>Sim, excluir</Button.Title>
-                    </Button> 
-                </ButtonsView>
-            </Container>
+                <Container>
+                    <Title>Deseja realmente excluir o registro da refeição ?</Title>
+                    <ButtonsView>
+                        <Button variant="secondary" onPress={onCancel}>
+                            <Button.Title>Cancelar</Button.Title>
+                        </Button>
+                        <Button onPress={onSuccess}>
+                            <Button.Title>Sim, excluir</Button.Title>
+                        </Button> 
+                    </ButtonsView>
+                </Container>
             </Overlay>
         </Modal>
     )
